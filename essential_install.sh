719aee3b9d393f3f65769b3e915b5e0f4d5cf7de
Shuaib Ahmed <shuaib88@bitbucket.org>
2017-11-05 21:00:24 -0600

    added echo statements

--- essential_install.sh
+++ essential_install.sh
@@ -16,24 +16,21 @@ git config --global user.name "$full_name"
 git config --global user.email "$email"
 ### TODO add ssh for username/password creds
 
-
 ### bash-completion
+echo "SETTING UP BASH COMPLETION"
 brew install bash-completion
 cp bash_profile ~/.bash_profile
 
-##
-##### cpy the relevant inputrc and bash_profile codes
-##
-
-## good tips for more advanced config 
-## https://gist.github.com/trey/2722934
-
 ### tmux
+echo "INSTALLING TMUX"
 brew install tmux
 cp tmux.conf ~/.tmux.conf
 ### TODO get pasting functionality working
 
-
 ### VIM
+echo "CONFIGURING VIM"
 cp vimrc ~/.vimrc
 ### TODO -> get pasting funcitonality working right
+
+## good tips for more advanced config 
+## https://gist.github.com/trey/2722934
